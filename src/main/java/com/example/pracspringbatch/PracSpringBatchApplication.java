83778322f1328d1cb632e4869bf5afc5722a29ca
kwongyumin <rbals7398@naver.com>
2023-10-30 22:53:29 +0900

    - 배치 테이블 생성 및 Job 테스트

--- src/main/java/com/example/pracspringbatch/PracSpringBatchApplication.java
+++ src/main/java/com/example/pracspringbatch/PracSpringBatchApplication.java
@@ -1,13 +1,17 @@
 package com.example.pracspringbatch;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class PracSpringBatchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PracSpringBatchApplication.class, args);
+        System.out.println("TEST");
     }
 
 }
