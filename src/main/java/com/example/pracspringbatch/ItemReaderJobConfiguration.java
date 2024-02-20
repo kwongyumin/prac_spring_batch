a6c305e9dac3ec8e53192b8da7cd26335fe129d3
kwongyumin <rbals7398@naver.com>
2024-02-20 20:56:55 +0900

    - JsonItemReader

--- src/main/java/com/example/pracspringbatch/ItemReaderJobConfiguration.java
+++ src/main/java/com/example/pracspringbatch/ItemReaderJobConfiguration.java
@@ -9,6 +9,10 @@ import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.transform.Range;
+import org.springframework.batch.item.json.JacksonJsonObjectReader;
+import org.springframework.batch.item.json.JsonItemReader;
+import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -26,10 +30,10 @@ public class ItemReaderJobConfiguration {
     }
 
     @Bean
-    public Step step(JobRepository jobRepository , PlatformTransactionManager platformTransactionManager, ItemReader<User> flatFileItemReader) {
+    public Step step(JobRepository jobRepository , PlatformTransactionManager platformTransactionManager, ItemReader<User> jsonItemReader) {
         return new StepBuilder("itemReaderStep", jobRepository)
                 .<User,User>chunk(2,platformTransactionManager)
-                .reader(flatFileItemReader)
+                .reader(jsonItemReader)
                 .writer(System.out::println)
                 .build();
     }
@@ -43,6 +47,30 @@ public class ItemReaderJobConfiguration {
                 .delimited().delimiter(",")
                 .names("name","age","region","telephone")
                 .targetType(User.class)
+                // .strict(false) -- 찾는 파일이 없을 시, exception 이 아닌 동작 종료
+                .build();
+    }
+
+    @Bean
+    public FlatFileItemReader<User> fixedLengthFlatFileItemReader() {
+        return new FlatFileItemReaderBuilder<User>()
+                .name("fixedLengthFlatFileItemReader")
+                .resource(new ClassPathResource("usersFixedLength.txt"))
+                .linesToSkip(2)
+                .fixedLength()
+                .columns(new Range[]{new Range(1,2) , new Range(3,4) , new Range(5,6) , new Range(7,19)})
+                .names("name","age","region","telephone")
+                .targetType(User.class)
+                // .strict(false) -- 찾는 파일이 없을 시, exception 이 아닌 동작 종료
+                .build();
+    }
+
+    @Bean
+    public JsonItemReader<User> jsonItemReader() {
+        return new JsonItemReaderBuilder<User>()
+                .name("jsonItemReader")
+                .resource(new ClassPathResource("users.json"))
+                .jsonObjectReader(new JacksonJsonObjectReader<>(User.class))
                 .build();
     }
 
